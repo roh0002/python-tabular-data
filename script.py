ec4fced311678e1c4507efdec687f47f485c3a0c
Rachel Haich <roh0002@auburn.edu>
2022-03-09 13:52:40 -0600

    Improved naming of file and plots

--- script.py
+++ script.py
@@ -1,12 +1,12 @@
 #! usr/bin/env python
-import os
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy import stats 
+from scipy import stats
 
-def regression():
-    """Makes a linear regression model based on the given iris species
+def regression(species_subset, i):
+    """Makes a linear regression model based on the given iris species, comparing petal length and sepal length.
+       Initial save name is "species.png", which can be changed in the regression function or outside using a rename function.
     """
     x = species_subset.petal_length_cm
     y = species_subset.sepal_length_cm
@@ -18,22 +18,21 @@ def regression():
     plt.xlabel("Petal length (cm)")
     plt.ylabel("Sepal length (cm)")
     plt.legend()
-    plt.savefig("species.png")
-
+    plt.title("%s" % i)
+    plt.savefig("%s.png" % i)
+    plt.clf()
 def species_regression():
     """
     Loops through unique species in dataset in order to run linear regression function.
-    Figures are renamed with appropriate species information and 
-    plt.clf is used to clear the plot between figures. 
+    Figures are renamed with appropriate species information and
+    plt.clf is used to clear the plot between figures.
     """
     species_list = dataset.species.unique()
     print(species_list)
     for i in species_list:
         print(i)
-        species_subset = dataset[dataset.species == i] 
-        regression()
-        os.rename('species.png', "regress_%s.png" % i)
-        plt.clf()
+        species_subset = dataset[dataset.species == i]
+        regression(species_subset, i)
 
 if __name__ == '__main__':
     dataset = pd.read_csv("iris.csv")
